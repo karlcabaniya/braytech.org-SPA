7dd9af3f3ddce36badc210b006039d7644c67180
Tom Chapman <chapman@live.com.au>
2020-04-15 15:27:25 +1000

    2.23.1

--- scripts/temp.mjs
+++ scripts/temp.mjs
@@ -23,38 +23,40 @@ const langs = ['en', 'de', 'es', 'fr', 'it', 'ja', 'pt-br'];
 
 langs.forEach(l => {
   
-  const def = JSON.parse(fs.readFileSync(`src/data/d1grimoire/${l}/DestinyGrimoireCardDefinition.json`));
+  // const def = JSON.parse(fs.readFileSync(`src/data/d1grimoire/${l}/DestinyGrimoireCardDefinition.json`));
 
-  Object.keys(def).forEach(key => {
+  // Object.keys(def).forEach(key => {
 
-    // def[key].cardDescription = def[key].cardDescription && entities.decodeHTML(turndownService.turndown(def[key].cardDescription));
-    // def[key].cardIntro = def[key].cardIntro && entities.decodeHTML(def[key].cardIntro.replace(/<\/?[^>]+(>|$)/g, ""));
-    // def[key].cardIntroAttribution = def[key].cardIntroAttribution && entities.decodeHTML(def[key].cardIntroAttribution.replace(/<\/?[^>]+(>|$)/g, ""));
+  //   // def[key].cardDescription = def[key].cardDescription && entities.decodeHTML(turndownService.turndown(def[key].cardDescription));
+  //   // def[key].cardIntro = def[key].cardIntro && entities.decodeHTML(def[key].cardIntro.replace(/<\/?[^>]+(>|$)/g, ""));
+  //   // def[key].cardIntroAttribution = def[key].cardIntroAttribution && entities.decodeHTML(def[key].cardIntroAttribution.replace(/<\/?[^>]+(>|$)/g, ""));
 
-    // if (def[key].cardIntroAttribution && def[key].cardIntroAttribution[0] === '-') {
-    //   def[key].cardIntroAttribution = def[key].cardIntroAttribution.replace('-','—');
-    // }
+  //   // if (def[key].cardIntroAttribution && def[key].cardIntroAttribution[0] === '-') {
+  //   //   def[key].cardIntroAttribution = def[key].cardIntroAttribution.replace('-','—');
+  //   // }
 
-    def[key].cardName = def[key].cardName && entities.decodeHTML(def[key].cardName);
+  //   def[key].cardName = def[key].cardName && entities.decodeHTML(def[key].cardName);
 
-  });
+  // });
   
-  fs.writeFileSync(`src/data/d1grimoire/${l}/DestinyGrimoireCardDefinition.json`, JSON.stringify(def, null, '  '));
+  // fs.writeFileSync(`src/data/d1grimoire/${l}/DestinyGrimoireCardDefinition.json`, JSON.stringify(def, null, '  '));
   
-  // const def = JSON.parse(fs.readFileSync(`src/data/d1grimoire/${l}/DestinyGrimoireDefinition.json`));
+  const def = JSON.parse(fs.readFileSync(`src/data/d1grimoire/${l}/DestinyGrimoireDefinition.json`));
 
-  // def.themeCollection = def.themeCollection.map(t => {
+  def.themeCollection = def.themeCollection.map(t => {
 
-  //   t.pageCollection = t.pageCollection.map(p => {
+    t.themeName = entities.decodeHTML(t.themeName)
 
-  //     p.pageName = entities.decodeHTML(p.pageName)
+    t.pageCollection = t.pageCollection.map(p => {
 
-  //     return p;
-  //   })
+      p.pageName = entities.decodeHTML(p.pageName)
 
-  //   return t;
-  // })
+      return p;
+    })
+
+    return t;
+  })
   
-  // fs.writeFileSync(`src/data/d1grimoire/${l}/DestinyGrimoireDefinition.json`, JSON.stringify(def, null, '  '));
+  fs.writeFileSync(`src/data/d1grimoire/${l}/DestinyGrimoireDefinition.json`, JSON.stringify(def, null, '  '));
   
 });
