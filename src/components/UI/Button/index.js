669c0d935455c08ba53cefb1b98e8e16bdb05b84
Tom Chapman <chapman@live.com.au>
2020-05-12 00:37:07 +1000

    2.24.36

--- src/components/UI/Button/index.js
+++ src/components/UI/Button/index.js
@@ -76,17 +76,17 @@ class DestinyKey extends React.Component {
 
 class Button extends React.Component {
   render() {
-    const { className, text, children, action, cta, disabled, lined, anchor, type } = this.props;
+    const { className, text, children, action, cta, disabled, lined, anchor, type, inverted } = this.props;
 
     if (anchor) {
       return (
-        <Link className={cx('button', className, { lined: lined, disabled: disabled })} onClick={action || undefined} to={this.props.to}>
+        <Link className={cx('button', className, { inverted, lined, disabled })} onClick={action || undefined} to={this.props.to}>
           {text ? <div className='text'>{text}</div> : children}
         </Link>
       );
     } else {
       return (
-        <button className={cx('button', className, { lined, disabled, cta })} onClick={action || undefined} type={type || 'button'}>
+        <button className={cx('button', className, { inverted, lined, disabled, cta })} onClick={action || undefined} type={type || 'button'}>
           {text ? <div className='text'>{text}</div> : children}
         </button>
       );
