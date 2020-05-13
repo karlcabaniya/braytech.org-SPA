082e583c8e560054c245b5b1a0747b887b344978
Tom Chapman <chapman@live.com.au>
2020-05-13 13:44:58 +1000

    2.24.44

--- src/data/questLines/index.js
+++ src/data/questLines/index.js
@@ -2,23 +2,23 @@ export const stepsWithRecords = [
   // From the Mouths of Babes
   {
     objectiveHash: 4280752187,
-    recordHash: 1396554507
+    recordHash: 1396554507,
   },
   // The Ascent
   {
     objectiveHash: 3837519245,
-    recordHash: 3026670632
+    recordHash: 3026670632,
   },
   // Notorious Hustle
   {
     objectiveHash: 1449268997,
-    recordHash: 1745790710
+    recordHash: 1745790710,
   },
   // The Best Offense
   {
     objectiveHash: 858053549,
-    recordHash: 1827105928
-  }
+    recordHash: 1827105928,
+  },
 ];
 
 export const rewardsQuestLineOverrides = {
@@ -26,209 +26,209 @@ export const rewardsQuestLineOverrides = {
   96594792: [
     {
       itemHash: 153979399,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // The Mountaintop
   161355101: [
     {
       itemHash: 3993415705,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // The Recluse
   1511127118: [
     {
       itemHash: 3354242550,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Luna's Howl
   1404963370: [
     {
       itemHash: 153979396,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Forging the Broadsword
   2799168771: [
     {
       itemHash: 1161276682,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Truth
   884082918: [
     {
       itemHash: 1201830623,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Ace of Spades
   3872598781: [
     {
       itemHash: 347366834,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Polaris Lance
   128447428: [
     {
       itemHash: 3413074534,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // MIDA Multi-tool
   2081553940: [
     {
       itemHash: 1331482397,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Sleeper Simulant
   716563284: [
     {
       itemHash: 4036115577,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Sturm
   213655744: [
     {
       itemHash: 2907129556,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Drang
   656317236: [
     {
       itemHash: 2362471600,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // MIDA Mini-tool
   357114969: [
     {
       itemHash: 3089417788,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Whisper of the Worm
   1923833285: [
     {
       itemHash: 1891561814,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // A Thousand Wings
   2412366792: [
     {
       itemHash: 3403859120,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Obsidian Dreams (emblem)
   109998695: [
     {
       itemHash: 2083630698,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Malfeasance
   2791741089: [
     {
       itemHash: 204878059,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Legend of Acrius
   1306722632: [
     {
       itemHash: 3580904580,
-      quantity: 1
+      quantity: 1,
     },
     {
       // The Emperor's Pleasure (Ornament)
       itemHash: 2912834745,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Thorn
   3080875461: [
     {
       itemHash: 3973202132,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // 21% Delirium
   2704319977: [
     {
       itemHash: 1600633250,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Oxygen SR3
   3857337041: [
     {
       itemHash: 3907337522,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // The Last Word
   2819937365: [
     {
       itemHash: 1364093401,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Izanagi's Burden
   958496866: [
     {
       itemHash: 3211806999,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Nightshade
   2966837012: [
     {
       itemHash: 792755504,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Breakneck
   799683368: [
     {
       itemHash: 324382200,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Loaded Question
   2723561280: [
     {
       itemHash: 580961571,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // Bad Juju
   1350461162: [
     {
       itemHash: 2816212794,
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // An Education
   2354690788: [
     {
       itemHash: 2984240082, // Eriana's Vow Catalyst
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
   // New Enhancements
   3383977090: [
     {
       itemHash: 4218954970, // Symmetry Catalyst
-      quantity: 1
-    }
+      quantity: 1,
+    },
   ],
 };
 
@@ -239,17 +239,17 @@ export const rewardsQuestLineOverridesShadowkeep = [
     rewards: [
       {
         itemHash: 3089417789,
-        quantity: 1
+        quantity: 1,
       },
       {
         itemHash: 484491717,
-        quantity: 1
+        quantity: 1,
       },
       {
         itemHash: 1196526889,
-        quantity: 7
-      }
-    ]
+        quantity: 7,
+      },
+    ],
   },
   // Faculties of the Skull
   {
@@ -257,9 +257,9 @@ export const rewardsQuestLineOverridesShadowkeep = [
     rewards: [
       {
         itemHash: 2232171099, // Deathbringer
-        quantity: 1
-      }
-    ]
+        quantity: 1,
+      },
+    ],
   },
   // What's This… What's This?
   {
@@ -267,9 +267,9 @@ export const rewardsQuestLineOverridesShadowkeep = [
     rewards: [
       {
         itemHash: 4103414242, // Divinity
-        quantity: 1
-      }
-    ]
+        quantity: 1,
+      },
+    ],
   },
   // The Back Way
   {
@@ -277,9 +277,9 @@ export const rewardsQuestLineOverridesShadowkeep = [
     rewards: [
       {
         itemHash: 2591746970, // Leviathan's Breath
-        quantity: 1
-      }
-    ]
+        quantity: 1,
+      },
+    ],
   },
   // Recovering The Past
   {
@@ -287,9 +287,9 @@ export const rewardsQuestLineOverridesShadowkeep = [
     rewards: [
       {
         itemHash: 3393519051, // Perfect Paradox
-        quantity: 1
-      }
-    ]
+        quantity: 1,
+      },
+    ],
   },
   // Enter the Vault
   {
@@ -297,9 +297,9 @@ export const rewardsQuestLineOverridesShadowkeep = [
     rewards: [
       {
         itemHash: 1665952087, // The Fourth Horseman
-        quantity: 1
-      }
-    ]
+        quantity: 1,
+      },
+    ],
   },
   // A Strong Arm
   {
@@ -307,13 +307,13 @@ export const rewardsQuestLineOverridesShadowkeep = [
     rewards: [
       {
         itemHash: 2415517654, // Bastion
-        quantity: 1
+        quantity: 1,
       },
       {
-        itemHash: 4273298922, // Bastion Catalyst 
-        quantity: 1
-      }
-    ]
+        itemHash: 4273298922, // Bastion Catalyst
+        quantity: 1,
+      },
+    ],
   },
   // A Fateful Gift
   {
@@ -321,61 +321,71 @@ export const rewardsQuestLineOverridesShadowkeep = [
     rewards: [
       {
         itemHash: 3512014804, // Lumina
-        quantity: 1
-      }
-    ]
+        quantity: 1,
+      },
+    ],
   },
   // Emergence
   {
     itemHash: 3715926175,
     rewards: [
       {
-        itemHash: 1395261499, // Xenophage 
-        quantity: 1
-      }
-    ]
-  }
+        itemHash: 1395261499, // Xenophage
+        quantity: 1,
+      },
+    ],
+  },
+  // Warmind Evaluation
+  {
+    itemHash: 1797229574,
+    rewards: [
+      {
+        itemHash: 1179141605, // Felwinter's Lie
+        quantity: 1,
+      },
+    ],
+  },
 ];
 
 export const setDataQuestLineOverrides = {
   213655744: [
     {
       trackingValue: 0,
-      itemHash: 213655744
+      itemHash: 213655744,
     },
     {
       trackingValue: 0,
-      itemHash: 196262242
+      itemHash: 196262242,
     },
     {
       trackingValue: 0,
-      itemHash: 196262243
+      itemHash: 196262243,
     },
     {
       trackingValue: 0,
-      itemHash: 3445216955
+      itemHash: 3445216955,
     },
     {
       trackingValue: 0,
-      itemHash: 196262240
+      itemHash: 196262240,
     },
     {
       trackingValue: 0,
-      itemHash: 196262241
+      itemHash: 196262241,
     },
     {
       trackingValue: 0,
-      itemHash: 196262246
+      itemHash: 196262246,
     },
     {
       trackingValue: 0,
-      itemHash: 196262247
-    }
+      itemHash: 196262247,
+    },
   ],
   2412366792: [
     {
       trackingValue: 0,
-      itemHash: '2412366792_enigmatic_blueprint'
-    }
-  ]
+      itemHash: '2412366792_enigmatic_blueprint',
+    },
+  ],
 };
