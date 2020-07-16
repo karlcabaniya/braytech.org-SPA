9b83191e4313f2ef5c4e1566474511a1092b294b
Tom Chapman <chapman@live.com.au>
2020-07-16 21:21:36 +1000

    2.27.32

--- src/components/RecordsAlmost/index.js
+++ src/components/RecordsAlmost/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { orderBy } from 'lodash';
 import cx from 'classnames';
 
@@ -11,141 +11,139 @@ import { enumerateRecordState } from '../../utils/destinyEnums';
 import { ProfileLink } from '../../components/ProfileLink';
 import Records from '../Records';
 
-class RecordsAlmost extends React.Component {
-  scrollToRecordRef = React.createRef();
+export default function RecordsAlmost({ sort, limit, selfLinkFrom = false, ...props }) {
+  const settings = useSelector((state) => state.settings);
+  const member = useSelector((state) => state.member);
 
-  render() {
-    const { settings, member, sort, limit, selfLinkFrom = false } = this.props;
-    const characterRecords = member && member.data.profile.characterRecords.data;
-    const profileRecords = member && member.data.profile.profileRecords.data.records;
+  const characterRecords = member.data?.profile.characterRecords.data;
+  const profileRecords = member.data?.profile.profileRecords.data.records;
 
-    let almost = [];
-    const ignores = [];
+  // build a list of extra ignores
 
-    // ignore collections badges
-    manifest.DestinyPresentationNodeDefinition[manifest.settings.destiny2CoreSettings.badgesRootNode].children.presentationNodes.forEach((child) => {
-      ignores.push(manifest.DestinyPresentationNodeDefinition[child.presentationNodeHash].completionRecordHash);
-      manifest.DestinyPresentationNodeDefinition[child.presentationNodeHash].children.presentationNodes.forEach((subchild) => {
-        ignores.push(manifest.DestinyPresentationNodeDefinition[subchild.presentationNodeHash].completionRecordHash);
-      });
+  const ignores = [];
+
+  // ignore collections badges
+  manifest.DestinyPresentationNodeDefinition[manifest.settings.destiny2CoreSettings.badgesRootNode].children.presentationNodes.forEach((child) => {
+    ignores.push(manifest.DestinyPresentationNodeDefinition[child.presentationNodeHash].completionRecordHash);
+    manifest.DestinyPresentationNodeDefinition[child.presentationNodeHash].children.presentationNodes.forEach((subchild) => {
+      ignores.push(manifest.DestinyPresentationNodeDefinition[subchild.presentationNodeHash].completionRecordHash);
     });
+  });
 
-    // ignore MMXIX bullshit
-    manifest.DestinyPresentationNodeDefinition[1002334440].children.records.forEach((record) => {
-      ignores.push(record.recordHash);
-    });
+  // ignore MMXIX bullshit
+  manifest.DestinyPresentationNodeDefinition[1002334440].children.records.forEach((record) => {
+    ignores.push(record.recordHash);
+  });
 
-    // ignore seal completion records
-    manifest.DestinyPresentationNodeDefinition[1652422747].children.presentationNodes.forEach((node) => {
-      const definitionSeal = manifest.DestinyPresentationNodeDefinition[node.presentationNodeHash];
-      ignores.push(definitionSeal.completionRecordHash);
-    });
+  // ignore seal completion records
+  manifest.DestinyPresentationNodeDefinition[1652422747].children.presentationNodes.forEach((node) => {
+    const definitionSeal = manifest.DestinyPresentationNodeDefinition[node.presentationNodeHash];
 
-    const records = {
-      ...profileRecords,
-      ...characterRecords[member.characterId].records,
-    };
+    ignores.push(definitionSeal.completionRecordHash);
+  });
 
-    Object.entries(records).forEach(([key, recordData]) => {
+  // collect all the records and prepare them for ordering
+
+  const records = Object.entries({
+    ...profileRecords,
+    ...characterRecords[member.characterId].records,
+  })
+    .map(([key, data]) => {
       const hash = +key;
 
       if (settings.itemVisibility.hideUnobtainableRecords && unobtainable.indexOf(hash) > -1) {
-        return;
+        return false;
       }
 
       if (settings.itemVisibility.hideDudRecords && duds.indexOf(hash) > -1) {
-        return;
+        return false;
       }
 
       if (!manifest.DestinyRecordDefinition[hash] || manifest.DestinyRecordDefinition[hash].redacted) {
-        return;
+        return false;
       }
 
       if (ignores.includes(hash)) {
-        return;
+        return false;
       }
 
-      const enumeratedState = enumerateRecordState(recordData.state);
+      const enumeratedState = enumerateRecordState(data.state);
 
-      if (enumeratedState.RecordRedeemed || !enumeratedState.ObjectiveNotCompleted) return;
-      if (settings.itemVisibility.hideInvisibleRecords && (enumeratedState.Obscured || enumeratedState.Invisible)) return;
+      if (enumeratedState.RecordRedeemed || !enumeratedState.ObjectiveNotCompleted) return false;
+      if (settings.itemVisibility.hideInvisibleRecords && (enumeratedState.Obscured || enumeratedState.Invisible)) return false;
 
-      let completionValueDiviser = 0;
-      let progressValueDecimal = 0;
+      const definitionRecord = manifest.DestinyRecordDefinition[hash];
 
-      if (recordData.intervalObjectives) {
-        const nextIndex = recordData.intervalObjectives.findIndex((o) => !o.complete);
+      const record = {
+        hash,
+        completionValueDiviser: 0,
+        progressValueDecimal: 0,
+        score: definitionRecord?.completionInfo?.ScoreValue || 0,
+        commonality: manifest.statistics.triumphs?.[definitionRecord.hash] || 0,
+        distance: 0,
+      };
+
+      if (data.intervalObjectives) {
+        const nextIndex = data.intervalObjectives.findIndex((o) => !o.complete);
         const lastIndex = nextIndex - 1 || 0;
 
-        if (!recordData.intervalObjectives[nextIndex]) return;
+        if (!data.intervalObjectives[nextIndex]) return false;
 
         // if (hash === 759958308) console.log(recordData, recordData.intervalObjectives[lastIndex], recordData.intervalObjectives[nextIndex]);
 
-        const progress = lastIndex > -1 ? recordData.intervalObjectives[nextIndex].progress - recordData.intervalObjectives[lastIndex].completionValue : recordData.intervalObjectives[nextIndex].progress;
-        const completionValue = lastIndex > -1 ? recordData.intervalObjectives[nextIndex].completionValue - recordData.intervalObjectives[lastIndex].completionValue : recordData.intervalObjectives[nextIndex].completionValue;
+        const progress = lastIndex > -1 ? data.intervalObjectives[nextIndex].progress - data.intervalObjectives[lastIndex].completionValue : data.intervalObjectives[nextIndex].progress;
+        const completionValue = lastIndex > -1 ? data.intervalObjectives[nextIndex].completionValue - data.intervalObjectives[lastIndex].completionValue : data.intervalObjectives[nextIndex].completionValue;
 
-        // console.log(progress, completionValue)
+        record.completionValueDiviser += 1;
+        record.progressValueDecimal += Math.min(progress / completionValue, 1);
 
-        completionValueDiviser += 1;
-        progressValueDecimal += Math.min(progress / completionValue, 1);
-      } else if (recordData.objectives) {
-        recordData.objectives.forEach((objective) => {
-          completionValueDiviser += 1;
-          progressValueDecimal += Math.min((objective.progress || 0) / (objective.completionValue || 1), 1);
+        record.score = definitionRecord.intervalInfo?.intervalObjectives?.[nextIndex]?.intervalScoreValue || record.score || 0;
+      } else if (data.objectives) {
+        data.objectives.forEach((objective) => {
+          record.completionValueDiviser += 1;
+          record.progressValueDecimal += Math.min((objective.progress || 0) / (objective.completionValue || 1), 1);
         });
       } else {
-        return;
+        return false;
       }
 
-      const distance = progressValueDecimal / completionValueDiviser;
+      record.distance = record.progressValueDecimal / record.completionValueDiviser;
 
-      if (distance >= completionValueDiviser) {
-        return;
+      if (record.distance >= record.completionValueDiviser) {
+        return false;
       }
 
-      const definitionRecord = manifest.DestinyRecordDefinition[hash];
-      const score = definitionRecord?.completionInfo?.ScoreValue || 0;
+      return record;
+    })
+    // filter out the falsies
+    .filter((record) => record);
 
-      // if (hash === 61221277) console.log(definitionRecord.displayProperties.name, distance, progressValueDecimal, completionValueDiviser);
+  // order the records
 
-      almost.push({
-        distance,
-        score,
-        commonality: manifest.statistics.triumphs?.[definitionRecord.hash] || 0,
-        recordHash: definitionRecord.hash,
-      });
-    });
+  let ordered = [];
 
-    if (sort === 1) {
-      almost = orderBy(almost, [(record) => record.score, (record) => record.distance], ['desc', 'desc']);
-    } else if (sort === 2) {
-      almost = orderBy(almost, [(record) => record.commonality, (record) => record.distance], ['desc', 'desc']);
-    } else {
-      almost = orderBy(almost, [(record) => record.distance, (record) => record.score], ['desc', 'desc']);
-    }
-
-    almost = limit ? almost.slice(0, limit) : almost;
-
-    return (
-      <>
-        <ul className={cx('list record-items')}>
-          <Records selfLinkFrom={selfLinkFrom} hashes={almost.map((record) => record.recordHash)} />
-        </ul>
-        {this.props.pageLink ? (
-          <ProfileLink className='button' to={{ pathname: '/triumphs/almost-complete', state: { from: '/triumphs' } }}>
-            <div className='text'>{t('See next {{limit}}', { limit: 200 })}</div>
-          </ProfileLink>
-        ) : null}
-      </>
-    );
+  if (sort === 1) {
+    ordered = orderBy(records, [(record) => record.score, (record) => record.distance], ['desc', 'desc']);
+  } else if (sort === 2) {
+    ordered = orderBy(records, [(record) => record.commonality, (record) => record.distance], ['desc', 'desc']);
+  } else {
+    ordered = orderBy(records, [(record) => record.distance, (record) => record.score], ['desc', 'desc']);
   }
-}
 
-function mapStateToProps(state) {
-  return {
-    settings: state.settings,
-    member: state.member,
-  };
-}
+  // limit so nothing melts
 
-export default connect(mapStateToProps)(RecordsAlmost);
+  ordered = limit ? ordered.slice(0, limit) : ordered;
+
+  return (
+    <>
+      <ul className={cx('list record-items')}>
+        <Records selfLinkFrom={selfLinkFrom} hashes={ordered.map((record) => record.hash)} />
+      </ul>
+      {props.pageLink ? (
+        <ProfileLink className='button' to={{ pathname: '/triumphs/almost-complete', state: { from: '/triumphs' } }}>
+          <div className='text'>{t('See next {{limit}}', { limit: 200 })}</div>
+        </ProfileLink>
+      ) : null}
+    </>
+  );
+}
