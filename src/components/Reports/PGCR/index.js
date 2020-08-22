1d91d5c429f25a348c28084c485542033473cf85
Tom Chapman <chapman@live.com.au>
2020-08-22 21:10:55 +1000

    deps, ReportItem class to hooks, valor resets bug fix
    [version=2.31.4]

--- src/components/Reports/PGCR/index.js
+++ src/components/Reports/PGCR/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useRef, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { orderBy, groupBy } from 'lodash';
 import cx from 'classnames';
 
+import actions from '../../../store/actions';
 import { t } from '../../../utils/i18n';
-import * as bungie from '../../../utils/bungie';
+import { GetProfile } from '../../../utils/bungie';
 import { GetElo } from '../../../utils/thirdPartyApis';
-import * as utils from '../../../utils/destinyUtils';
+import { calculateResets } from '../../../utils/destinyUtils';
 import { Button, DestinyKey } from '../../UI/Button';
 import MemberLink from '../../MemberLink';
 
@@ -20,296 +21,248 @@ const unfinishableActivityModes = [
   76, // Reckoning
 ];
 
-const headInViewport = function (element) {
+function headInViewport(element) {
   const bounding = element.getBoundingClientRect();
 
   return bounding.top >= 0;
-};
+}
 
-class ReportItem extends React.Component {
-  state = {
-    expandedReport: Boolean(this.props.expanded),
-    expandedPlayers: [],
-    playerCache: [],
-  };
+export default function ReportItem({ report, expanded }) {
+  const member = useSelector((state) => state.member);
+  const dispatch = useDispatch();
+  const ref_parent = useRef();
 
-  ref_parent = React.createRef();
+  const [isExpanded, setExpanded] = useState(expanded && true);
+  const [expandedPlayers, setExpandedPlayers] = useState([]);
+  const [playerCache, setPlayerCache] = useState([]);
 
-  handler_expand = (e) => {
-    this.setState({
-      expandedReport: true,
-    });
+  useEffect(() => {
+    if (isExpanded) {
+      updatePlayerCache();
+    }
+  }, []);
 
-    this.updatePlayerCache();
-  };
+  useEffect(() => {
+    dispatch(actions.tooltips.rebind());
+  }, [expandedPlayers]);
 
-  handler_contract = (e) => {
-    this.setState({
-      expandedReport: false,
-      expandedPlayers: [],
-    });
+  function handler_expand(event) {
+    setExpanded(true);
 
-    if (!headInViewport(this.ref_parent.current)) {
-      this.ref_parent.current.scrollIntoView({ behavior: 'smooth' });
+    updatePlayerCache();
+  }
+
+  function handler_contract(event) {
+    setExpanded(false);
+    setExpandedPlayers([]);
+
+    if (!headInViewport(ref_parent.current)) {
+      ref_parent.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  const handler_togglePlayer = (characterId) => (event) => {
+    if (expandedPlayers.includes(characterId)) {
+      setExpandedPlayers((state) => [...state.filter((c) => c !== characterId)]);
+    } else {
+      setExpandedPlayers((state) => [...state, characterId]);
     }
   };
 
-  updatePlayerCache = () => {
-    const { report } = this.props;
-
+  function updatePlayerCache() {
     if (report) {
-      report.entries.forEach(async (e) => {
-        const progression = await this.getProgression(e.player.destinyUserInfo.membershipType, e.player.destinyUserInfo.membershipId, e.characterId);
+      report.entries.forEach(async (entry) => {
+        const { membershipType, membershipId } = entry.player.destinyUserInfo;
 
-        if (this.mounted) {
-          this.setState((p) => ({
-            ...p,
-            playerCache: [
-              ...p.playerCache,
-              {
-                membershipId: e.player.destinyUserInfo.membershipId,
-                ...progression,
-              },
-            ],
-          }));
-        }
+        if (!membershipType || !membershipId) return;
+
+        const progression = await getProgression(membershipType, membershipId, report.activityDetails.mode);
+
+        // if (this.mounted) {
+        setPlayerCache((state) => [
+          ...state,
+          {
+            membershipId,
+            ...progression,
+          },
+        ]);
+        // }
       });
     }
-  };
+  }
 
-  getElo = async (membershipId, mode, season) => {
-    const response = await GetElo({ params: { membershipId, mode, season } });
+  const characters = member.data && member.data.profile.characters.data;
+  const characterIds = characters && characters.map((c) => c.characterId);
 
-    if (response) {
-      return response.find((m) => m.mode === mode) || {};
-    }
+  const isFinishable = !unfinishableActivityModes.filter((mode) => report.activityDetails.modes.indexOf(mode) > -1).length;
 
-    return {};
-  };
+  if (isExpanded) console.log(report);
 
-  getProgression = async (membershipType, membershipId) => {
-    const response = await bungie.GetProfile({
-      params: {
-        membershipType,
-        membershipId,
-        components: '202,900',
-      },
-    });
+  const entry = characterIds && report.entries.find((entry) => characterIds.includes(entry.characterId));
+  const standing = entry && entry.values.standing && entry.values.standing.basic.value !== undefined ? entry.values.standing.basic.value : -1;
 
-    if (!response || (response && response.ErrorCode !== 1) || (response && response.ErrorCode === 1 && !response.Response.characterProgressions.data)) {
-      return {
-        points: {},
-        resets: {},
-        trials: {},
-        elo: {},
-      };
-    }
-
-    const elo = this.props.report.activityDetails.mode === 84 && (await this.getElo(membershipId, 84, 10));
-
-    // in case the PGCR refers to a character that has since been deleted, as is the case with "5364501167"
-    const characterId = Object.keys(response.Response.characterProgressions.data)[0];
-
-    const characterProgressions = response.Response.characterProgressions.data;
-    const characterRecords = response.Response.characterRecords.data;
-    const profileRecords = response.Response.profileRecords.data.records;
-
-    const gloryPoints = characterProgressions[characterId].progressions[2000925172].currentProgress;
-    const valorPoints = characterProgressions[characterId].progressions[2626549951].currentProgress;
-    const infamyPoints = characterProgressions[characterId].progressions[2772425241].currentProgress;
-    const trialsWins = characterProgressions[characterId].progressions[1062449239].level;
-    const trialsLosses = characterProgressions[characterId].progressions[2093709363].level;
+  const entries = report.entries.map((entry) => {
+    const dnf = entry.values.completed.basic.value === 0 && isFinishable ? true : false;
+    const isExpandedPlayer = expandedPlayers.includes(entry.characterId);
 
     return {
-      points: {
-        glory: gloryPoints,
-        valor: valorPoints,
-        infamy: infamyPoints,
-      },
-      resets: {
-        valor: utils.calculateResets(3882308435, characterId, characterProgressions, characterRecords, profileRecords).resetsTotal,
-        infamy: utils.calculateResets(2772425241, characterId, characterProgressions, characterRecords, profileRecords).resetsTotal,
-      },
-      trials: {
-        wins: trialsWins,
-        losses: trialsLosses,
-      },
-      elo,
+      teamId: report.teams && report.teams.length ? entry.values.team.basic.value : null,
+      fireteamId: entry.values.fireteamId ? entry.values.fireteamId.basic.value : null,
+      element: (
+        <li key={entry.characterId} className={cx({ expanded: isExpandedPlayer })}>
+          <ul>
+            <li className={cx('linked', 'inline', { dnf: dnf })} onClick={handler_togglePlayer(entry.characterId)}>
+              <div className='member'>
+                <MemberLink type={entry.player.destinyUserInfo.membershipType} id={entry.player.destinyUserInfo.membershipId} displayName={entry.player.destinyUserInfo.displayName} characterId={entry.characterId} />
+              </div>
+              <EntryHeader activityDetails={report.activityDetails} entry={entry} playerCache={playerCache} />
+            </li>
+            {isExpandedPlayer ? (
+              <li>
+                <EntryDetail activityDetails={report.activityDetails} entry={entry} playerCache={playerCache} />
+              </li>
+            ) : null}
+          </ul>
+        </li>
+      ),
     };
-  };
+  });
 
-  handler_togglePlayer = (characterId) => (e) => {
-    const { expandedPlayers } = this.state;
+  const body = (
+    <>
+      <ReportHeaderLarge characterIds={characterIds} {...report} />
+      <div className='entries'>
+        {report.teams && report.teams.length ? (
+          orderBy(report.teams, [(t) => t.score.basic.value], ['desc']).map((team) => {
+            const fireteams = Object.values(
+              groupBy(
+                entries.filter((e) => e.teamId === team.teamId),
+                'fireteamId'
+              )
+            );
 
-    if (expandedPlayers.includes(characterId)) {
-      this.setState((p) => ({
-        ...p,
-        expandedPlayers: [...p.expandedPlayers.filter((c) => c !== characterId)],
-      }));
-    } else {
-      this.setState((p) => ({
-        ...p,
-        expandedPlayers: [...p.expandedPlayers, characterId],
-      }));
-    }
-  };
-
-  componentDidMount() {
-    this.mounted = true;
-
-    if (this.props.expanded) {
-      this.updatePlayerCache();
-    }
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  componentDidUpdate(p, s) {
-    if (s.expandedReport !== this.state.expandedReport || s.expandedPlayers !== this.state.expandedPlayers) {
-      this.props.rebindTooltips();
-    }
-  }
-
-  render() {
-    const { member, report, expanded } = this.props;
-
-    const characters = member.data && member.data.profile.characters.data;
-    const characterIds = characters && characters.map((c) => c.characterId);
-
-    const { expandedReport, expandedPlayers, playerCache } = this.state;
-
-    const isFinishable = !unfinishableActivityModes.filter((mode) => report.activityDetails.modes.indexOf(mode) > -1).length;
-
-    if (expandedReport) console.log(this.props);
-
-    const entry = characterIds && report.entries.find((entry) => characterIds.includes(entry.characterId));
-    const standing = entry && entry.values.standing && entry.values.standing.basic.value !== undefined ? entry.values.standing.basic.value : -1;
-
-    const entries = report.entries.map((entry) => {
-      const dnf = entry.values.completed.basic.value === 0 && isFinishable ? true : false;
-      const isExpandedPlayer = expandedPlayers.includes(entry.characterId);
-
-      return {
-        teamId: report.teams && report.teams.length ? entry.values.team.basic.value : null,
-        fireteamId: entry.values.fireteamId ? entry.values.fireteamId.basic.value : null,
-        element: (
-          <li key={entry.characterId} className={cx({ expanded: isExpandedPlayer })}>
-            <ul>
-              <li className={cx('linked', 'inline', { dnf: dnf })} onClick={this.handler_togglePlayer(entry.characterId)}>
-                <div className='member'>
-                  <MemberLink type={entry.player.destinyUserInfo.membershipType} id={entry.player.destinyUserInfo.membershipId} displayName={entry.player.destinyUserInfo.displayName} characterId={entry.characterId} />
-                </div>
-                <EntryHeader activityDetails={report.activityDetails} entry={entry} playerCache={playerCache} />
-              </li>
-              {isExpandedPlayer ? (
-                <li>
-                  <EntryDetail activityDetails={report.activityDetails} entry={entry} playerCache={playerCache} />
+            return (
+              <ul key={team.teamId} className='team'>
+                <li className={cx('header team', team.teamId === 17 ? 'alpha' : 'bravo')}>
+                  <div className='team name'>{team.teamId === 17 ? t('Alpha team') : t('Bravo team')}</div>
+                  <EntryHeader activityDetails={report.activityDetails} team />
+                  <div className='team score hideInline'>{team.score.basic.displayValue}</div>
                 </li>
-              ) : null}
-            </ul>
-          </li>
-        ),
-      };
-    });
-
-    const body = (
-      <>
-        <ReportHeaderLarge characterIds={characterIds} {...report} />
-        <div className='entries'>
-          {report.teams && report.teams.length ? (
-            orderBy(report.teams, [(t) => t.score.basic.value], ['desc']).map((team) => {
-              const fireteams = Object.values(
-                groupBy(
-                  entries.filter((e) => e.teamId === team.teamId),
-                  'fireteamId'
-                )
-              );
-
-              return (
-                <ul key={team.teamId} className='team'>
-                  <li className={cx('header team', team.teamId === 17 ? 'alpha' : 'bravo')}>
-                    <div className='team name'>{team.teamId === 17 ? t('Alpha team') : t('Bravo team')}</div>
-                    <EntryHeader activityDetails={report.activityDetails} team />
-                    <div className='team score hideInline'>{team.score.basic.displayValue}</div>
-                  </li>
-                  {fireteams.map((f, i) => {
-                    return (
-                      <li key={i}>
-                        <ul className={cx('list', 'fireteam', { stacked: f.length > 1 })}>{f.map((e) => e.element)}</ul>
-                      </li>
-                    );
-                  })}
-                </ul>
-              );
-            })
-          ) : (
-            <ul className='team'>
-              <li className={cx('header team')}>
-                <div className='team name' />
-                <EntryHeader activityDetails={report.activityDetails} team />
-                <div className='team score hideInline' />
-              </li>
-              {Object.values(groupBy(entries, 'fireteamId')).map((f, i) => {
-                return (
-                  <li key={i}>
-                    <ul className={cx('list', 'fireteam', { stacked: f.length > 1 })}>{f.map((e) => e.element)}</ul>
-                  </li>
-                );
-              })}
-            </ul>
-          )}
-        </div>
-        {!expanded && (
-          <div className='sticky-nav inline'>
-            <div className='wrapper'>
-              <div />
-              <ul>
-                <li>
-                  <a className='button' href={`/pgcr/${report.activityDetails.instanceId}`} target='_blank'>
-                    <DestinyKey type='more' />
-                    {t('New tab')}
-                  </a>
-                </li>
-                <li>
-                  <Button action={this.handler_contract}>
-                    <DestinyKey type='dismiss' />
-                    {t('Close')}
-                  </Button>
-                </li>
+                {fireteams.map((f, i) => {
+                  return (
+                    <li key={i}>
+                      <ul className={cx('list', 'fireteam', { stacked: f.length > 1 })}>{f.map((e) => e.element)}</ul>
+                    </li>
+                  );
+                })}
               </ul>
-            </div>
-          </div>
+            );
+          })
+        ) : (
+          <ul className='team'>
+            <li className={cx('header team')}>
+              <div className='team name' />
+              <EntryHeader activityDetails={report.activityDetails} team />
+              <div className='team score hideInline' />
+            </li>
+            {Object.values(groupBy(entries, 'fireteamId')).map((f, i) => {
+              return (
+                <li key={i}>
+                  <ul className={cx('list', 'fireteam', { stacked: f.length > 1 })}>{f.map((e) => e.element)}</ul>
+                </li>
+              );
+            })}
+          </ul>
         )}
-      </>
-    );
+      </div>
+      {!expanded && (
+        <div className='sticky-nav inline'>
+          <div className='wrapper'>
+            <div />
+            <ul>
+              <li>
+                <a className='button' href={`/pgcr/${report.activityDetails.instanceId}`} target='_blank'>
+                  <DestinyKey type='more' />
+                  {t('New tab')}
+                </a>
+              </li>
+              <li>
+                <Button action={handler_contract}>
+                  <DestinyKey type='dismiss' />
+                  {t('Close')}
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
 
-    return (
-      <li key={report.activityDetails.instanceId} ref={this.ref_parent} className={cx('linked', { isExpanded: expandedReport, standing: standing > -1, victory: standing === 0 })} onClick={!expandedReport ? this.handler_expand : undefined}>
-        {!expandedReport ? <ReportHeader characterIds={characterIds} {...report} /> : body}
-      </li>
-    );
+  return (
+    <li key={report.activityDetails.instanceId} ref={ref_parent} className={cx('linked', { isExpanded, standing: standing > -1, victory: standing === 0 })} onClick={!isExpanded ? handler_expand : undefined}>
+      {!isExpanded ? <ReportHeader characterIds={characterIds} {...report} /> : body}
+    </li>
+  );
+}
+
+async function getElo(membershipId, mode, season) {
+  const response = await GetElo({ params: { membershipId, mode, season } });
+
+  if (response) {
+    return response.find((m) => m.mode === mode) || {};
   }
+
+  return {};
 }
 
-function mapStateToProps(state, ownProps) {
-  return {
-    member: state.member,
-    viewport: state.viewport,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    rebindTooltips: (value) => {
-      dispatch({ type: 'REBIND_TOOLTIPS', payload: new Date().getTime() });
+async function getProgression(membershipType, membershipId, mode) {
+  const response = await GetProfile({
+    params: {
+      membershipType,
+      membershipId,
+      components: '202,900',
     },
+  });
+
+  if (!response || (response && response.ErrorCode !== 1) || (response && response.ErrorCode === 1 && !response.Response.characterProgressions.data)) {
+    return {
+      points: {},
+      resets: {},
+      trials: {},
+      elo: {},
+    };
+  }
+
+  const elo = mode === 84 && (await getElo(membershipId, 84, 10));
+
+  // in case the PGCR refers to a character that has since been deleted, as is the case with "5364501167"
+  const characterId = Object.keys(response.Response.characterProgressions.data)[0];
+
+  const characterProgressions = response.Response.characterProgressions.data;
+  const characterRecords = response.Response.characterRecords.data;
+  const profileRecords = response.Response.profileRecords.data.records;
+
+  const gloryPoints = characterProgressions[characterId].progressions[2000925172].currentProgress;
+  const valorPoints = characterProgressions[characterId].progressions[2626549951].currentProgress;
+  const infamyPoints = characterProgressions[characterId].progressions[2772425241].currentProgress;
+  const trialsWins = characterProgressions[characterId].progressions[1062449239].level;
+  const trialsLosses = characterProgressions[characterId].progressions[2093709363].level;
+
+  return {
+    points: {
+      glory: gloryPoints,
+      valor: valorPoints,
+      infamy: infamyPoints,
+    },
+    resets: {
+      valor: calculateResets(3882308435, characterId, characterProgressions, characterRecords, profileRecords).resetsTotal,
+      infamy: calculateResets(2772425241, characterId, characterProgressions, characterRecords, profileRecords).resetsTotal,
+    },
+    trials: {
+      wins: trialsWins,
+      losses: trialsLosses,
+    },
+    elo,
   };
 }
-
-ReportItem = connect(mapStateToProps, mapDispatchToProps)(ReportItem);
-
-export default ReportItem;
-
-export { ReportItem };
